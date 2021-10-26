9187955e856e49d7e4caf01f8fa8459d39720f81
philippjenny <42078113+philippjenny@users.noreply.github.com>
2021-10-26 12:25:19 +0200

    Narrow down eventlistener to galley

--- js/animationen.js
+++ js/animationen.js
@@ -13,13 +13,30 @@ window.addEventListener('load', function () {
   })
 })
 
-document.addEventListener('click', function(e){
+document.body.querySelectorAll('.gallery')[0].addEventListener('click', function(e){
   const image = e.target;
   const imageClicked = e.target.parentElement;
   const imageLarge = document.querySelector('.image-large img');
-
+  
   console.log(imageClicked);
-
-  document.querySelector('.image-large').appendChild(image);
-  imageClicked.appendChild(imageLarge)  
+  if(image === imageLarge){
+    return;
+  }else{
+    document.querySelector('.image-large').appendChild(image);
+    anime({
+      targets: image,
+      translateY: [100, 0],
+      opacity: [0, 1],
+      duration: 500,
+      easing: 'easeInOutSine'
+    });
+    imageClicked.appendChild(imageLarge)  
+    anime({
+      targets: imageLarge,
+      translateY: [-50, 0],
+      opacity: [0, 1],
+      duration: 500,
+      easing: 'easeInOutBack',
+    });
+  }
 })
