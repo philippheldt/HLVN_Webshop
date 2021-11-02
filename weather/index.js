87c038ebdcf11258faec37f1f6381d4d9f41d04f
philippjenny <42078113+philippjenny@users.noreply.github.com>
2021-11-02 16:26:21 +0100

    Revert changes API

--- weather/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const fetch = require("node-fetch");
-
-router.get("/", (req,res)=> {
-    res.json({success: "Hello Weather!"})
-})
